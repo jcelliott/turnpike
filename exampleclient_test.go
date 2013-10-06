4660d2287cc2124d87c3f71ff1c6c17908e2fb04
Max Persson <max@maxpersson.com>
2013-10-06 18:34:12 +0200

    Simplify and fix client example

--- exampleclient_test.go
+++ exampleclient_test.go
@@ -1,17 +1,14 @@
 package turnpike_test
 
 import (
-	"code.google.com/p/go.net/websocket"
 	"github.com/jcelliott/turnpike"
-	"net/http"
 )
 
 func ExampleClient_NewClient() {
 	c := turnpike.NewClient()
 	err := c.Connect("ws://127.0.0.1:8080/ws", "http://localhost/")
 	if err != nil {
-		fmt.Println("Error connecting:", err)
-		return
+		panic("Error connecting:" + err.Error())
 	}
 
 	c.Call("rpc:test")
