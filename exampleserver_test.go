112023e78d2106f04ccbe68312e41a26bfb2738f
Max Persson <max@maxpersson.com>
2013-10-06 18:37:51 +0200

    Simplify server example

--- exampleserver_test.go
+++ exampleserver_test.go
@@ -1,16 +1,14 @@
 package turnpike_test
 
 import (
-	"code.google.com/p/go.net/websocket"
 	"github.com/jcelliott/turnpike"
 	"net/http"
 )
 
 func ExampleServer_NewServer() {
 	s := turnpike.NewServer()
-	ws := websocket.Handler(s.HandleWebsocket)
 
-	http.Handle("/ws", ws)
+	http.Handle("/ws", s.Handler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
