96dafb3c057ab420488adca86aad1f01c7447bce
Max Persson <max@maxpersson.com>
2013-10-09 16:57:36 +0200

    Add tests for publish handler

--- server_test.go
+++ server_test.go
@@ -162,3 +162,86 @@ func TestServer_SubHandlerDeny(t *testing.T) {
 	c.Publish("event:test", "test")
 	<-time.After(time.Second)
 }
+
+func TestServer_RegisterPubHandler(t *testing.T) {
+	s := NewServer()
+	pubCh := make(chan bool)
+	s.RegisterPubHandler("event:test", func(topicURI string, event interface{}) interface{} {
+		pubCh <- true
+		return event
+	})
+
+	http.Handle("/ws_s5", s.Handler)
+	// TODO: needs better way of running multiple listen and serve.
+	// Currently there is no way of closing the listener. A cusom server and
+	// handler will work but requires more work. TBD.
+	go func() {
+		err := http.ListenAndServe(":8105", nil)
+		if err != nil {
+			t.Fatal("ListenAndServe: " + err.Error())
+		}
+	}()
+
+	// Let the server goroutine start.
+	runtime.Gosched()
+
+	c := NewClient()
+	err := c.Connect("ws://127.0.0.1:8105/ws_s5", "http://localhost/")
+	if err != nil {
+		t.Fatal("error connecting: " + err.Error())
+	}
+
+	// c.Subscribe("event:test", func(uri string, event interface{}) {})
+	c.Publish("event:test", "test")
+
+	select {
+	case <-pubCh:
+		return
+	case <-time.After(time.Second):
+		t.Fail()
+	}
+}
+
+func TestServer_PubHandlerChange(t *testing.T) {
+	s := NewServer()
+	s.RegisterPubHandler("event:test", func(topicURI string, event interface{}) interface{} {
+		return event.(string) + "2"
+	})
+
+	http.Handle("/ws_s6", s.Handler)
+	// TODO: needs better way of running multiple listen and serve.
+	// Currently there is no way of closing the listener. A cusom server and
+	// handler will work but requires more work. TBD.
+	go func() {
+		err := http.ListenAndServe(":8106", nil)
+		if err != nil {
+			t.Fatal("ListenAndServe: " + err.Error())
+		}
+	}()
+
+	// Let the server goroutine start.
+	runtime.Gosched()
+
+	c := NewClient()
+	err := c.Connect("ws://127.0.0.1:8106/ws_s6", "http://localhost/")
+	if err != nil {
+		t.Fatal("error connecting: " + err.Error())
+	}
+
+	eventCh := make(chan bool)
+	c.Subscribe("event:test", func(uri string, event interface{}) {
+		if event != "test2" {
+			t.Fail()
+		}
+		eventCh <- true
+	})
+
+	c.Publish("event:test", "test")
+
+	select {
+	case <-eventCh:
+		return
+	case <-time.After(time.Second):
+		t.Fail()
+	}
+}
