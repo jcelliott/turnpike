d50f251311b6e0ade1f09afe214feb57f41c56a6
Marcel Hauf <mail@marcelhauf.name>
2015-07-15 10:51:50 +0200

    Add test for Client.Unregister

--- client_test.go
+++ client_test.go
@@ -62,6 +62,24 @@ func (t *testPeer) Send(msg Message) error {
 				ArgumentsKw: make(map[string]interface{}),
 			}
 		}
+	case *Unregister:
+		// Only allow methods name "mymethod" (4567) to be unregistered
+		if msg.Registration == 4567 {
+			args := make([]interface{}, 0)
+			args = append(args, 1234)
+
+			t.messages <- &Unregistered{
+				Request: msg.Request,
+			}
+		} else {
+			t.messages <- &Error{
+				Type:        UNREGISTER,
+				Request:     msg.Request,
+				Error:       WAMP_ERROR_INVALID_URI,
+				Arguments:   make([]interface{}, 0),
+				ArgumentsKw: make(map[string]interface{}),
+			}
+		}
 
 	case *Yield:
 		// Transform the yield into a result, and send it back to the client.
@@ -184,22 +202,47 @@ func TestRemoteCall(t *testing.T) {
 			})
 		})
 
+		Convey("The callee unregisters an invalid method", func() {
+			err := callee.Unregister("invalidmethod")
+			Convey("And expects an error", func() {
+				So(err, ShouldNotBeNil)
+			})
+		})
+
 		Convey("The callee registers a valid method", func() {
 			handler := func(args []interface{}, kwargs map[string]interface{}) *CallResult {
 				return &CallResult{Args: []interface{}{args[0].(int) * 2}}
 			}
-			err := callee.Register("mymethod", handler)
+			methodName := "mymethod"
+			err := callee.Register(methodName, handler)
 
 			Convey("And expects no error", func() {
 				So(err, ShouldBeNil)
 
 				Convey("The caller calls the callee's remote method", func() {
 					callArgs := []interface{}{5100}
-					result, err := caller.Call("mymethod", callArgs, make(map[string]interface{}))
+					result, err := caller.Call(methodName, callArgs, make(map[string]interface{}))
 
 					Convey("And succeeds at multiplying the number by 2", func() {
 						So(err, ShouldBeNil)
 						So(result.(*Result).Arguments[0], ShouldEqual, 10200)
+					})
+				})
+			})
+
+			Convey("And unregisters the method", func() {
+				err := callee.Unregister(methodName)
+				Convey("And expects no error", func() {
+					So(err, ShouldBeNil)
+				})
+
+				Convey("Calling the unregistered procedure", func() {
+					callArgs := []interface{}{5100}
+					result, err := caller.Call(methodName, callArgs, make(map[string]interface{}))
+
+					Convey("Should result in an error", func() {
+						So(err, ShouldNotBeNil)
+						So(result, ShouldBeNil)
 					})
 				})
 			})
