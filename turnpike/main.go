9ec6a8adecb838c8031c03bc85f80de6d88b0ad3
Joshua Elliott <joshuacelliott@gmail.com>
2015-09-06 23:35:20 -0600

    Add basic `turnpike` command
    * go install gopkg.in/jcelliott/turnpike.v2/turnpike
    * $GOPATH/bin/turnpike

---
+++ turnpike/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"gopkg.in/jcelliott/turnpike.v2"
+)
+
+var (
+	realm string
+	port  int
+)
+
+func init() {
+	flag.StringVar(&realm, "realm", "realm1", "realm name")
+	flag.IntVar(&port, "port", 8000, "port to run on")
+}
+
+func main() {
+	flag.Parse()
+	s := turnpike.NewBasicWebsocketServer(realm)
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	go func() {
+		<-shutdown
+		s.Close()
+		log.Println("shutting down server...")
+		time.Sleep(time.Second)
+		os.Exit(1)
+	}()
+
+	server := &http.Server{
+		Handler: s,
+		Addr:    ":8000",
+	}
+	log.Printf("turnpike server starting on port %d...", port)
+	log.Fatal(server.ListenAndServe())
+}
