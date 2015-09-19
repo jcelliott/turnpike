f42a515aa4a42727a1fed723e2a42a3faba577d3
Joshua Elliott <joshuacelliott@gmail.com>
2015-09-19 14:23:21 -0600

    Add debug flag to turnpike command

--- turnpike/main.go
+++ turnpike/main.go
@@ -14,15 +14,20 @@ import (
 var (
 	realm string
 	port  int
+	debug bool
 )
 
 func init() {
 	flag.StringVar(&realm, "realm", "realm1", "realm name")
 	flag.IntVar(&port, "port", 8000, "port to run on")
+	flag.BoolVar(&debug, "debug", false, "enable debug logging")
 }
 
 func main() {
 	flag.Parse()
+	if debug {
+		turnpike.Debug()
+	}
 	s := turnpike.NewBasicWebsocketServer(realm)
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, os.Interrupt)
