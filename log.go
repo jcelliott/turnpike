42f7fb994e37066781921a14c57409dd75b285ed
Joshua Elliott <joshuacelliott@gmail.com>
2015-10-12 15:36:54 -0600

    Add DebugOff function to switch to noop logger

--- log.go
+++ log.go
@@ -35,6 +35,11 @@ func Debug() {
 	log = glog.New(os.Stderr, "", logFlags)
 }
 
+// DebugOff changes the log to a noop logger
+func DebugOff() {
+	log = noopLogger{}
+}
+
 // SetLogger allows users to inject their own logger instead of the default one.
 func SetLogger(l Logger) {
 	log = l
