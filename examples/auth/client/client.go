6543e7d830141951834eae2687034d39c21a471b
mingang.he <dustgle@gmail.com>
2016-09-09 22:49:55 +0800

    Update auth example: gopass.GetPasswd() return ([]byte, error)

--- examples/auth/client/client.go
+++ examples/auth/client/client.go
@@ -29,7 +29,11 @@ func main() {
 	turnpike.Debug()
 	fmt.Println("Hint: the password is 'password'")
 	fmt.Print("Password: ")
-	password = gopass.GetPasswd()
+	var err error
+	password, err = gopass.GetPasswd()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	c, err := turnpike.NewWebsocketClient(turnpike.JSON, "ws://localhost:8000/ws", nil)
 	if err != nil {
